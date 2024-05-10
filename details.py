166f726af45999b8ddedd792b3f871af09dacaa2
rohit99504 <163815811+rohit99504@users.noreply.github.com>
2024-05-10 09:26:27 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
 api_id = 27498866
 api_hash = "96fbb6ad2e11ab04e83ca09ef3f42455"
 bot_token = "7166257969:AAFbVtNiIEPqgaH8mDW3ymED0P7bJPAhZ-k"
-sudo_groups = -1002059340064
+sudo_groups = -4212260142
